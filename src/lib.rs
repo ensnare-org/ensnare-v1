4debcc49043f7149d0e2e02913eec097aefd4215
Mike Tsao <mike@sowbug.com>
2023-09-20 12:07:01 -0700

    Add control module

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,9 @@
 
 //! Ensnare is a library for generating digital audio.
 
+/// Handles automation, or real-time automatic control of one entity's
+/// parameters by another entity's output.
+pub mod control;
 /// Contains common structures and constants used across the library.
 pub mod core;
 /// Handles digital-audio, wall-clock, and musical time.
@@ -13,6 +16,7 @@ pub mod uid;
 
 /// Recommended imports for easy onboarding.
 pub mod prelude {
+    pub use crate::control::{ControlIndex, ControlName, ControlValue};
     pub use crate::core::{
         BipolarNormal, FrequencyHz, Normal, ParameterType, Ratio, Sample, SampleType, SignalType,
         StereoSample,
