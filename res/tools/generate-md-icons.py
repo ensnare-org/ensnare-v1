aee5a6dfec81ac88da50e608ae941a2f3f85867a
Mike Tsao <mike@sowbug.com>
2023-11-17 11:46:30 -0800

    Add piano icon, mogrify to strip PNG timestamps

--- res/tools/generate-md-icons.py
+++ res/tools/generate-md-icons.py
@@ -4,14 +4,14 @@
 # suitable to use in this app.
 #
 # To use:
-# 
+#
 # 1. Clone the repo
 #    [material-design-icons](https://github.com/google/material-design-icons/)
 #    and unzip somewhere. In these instructions we'll assume the location is
 #    ~/Downloads/material-design-icons.
 # 2. cd to the root of this source tree.
 # 3. res/tools/generate-md-icons.py ~/Downloads/material-design-icons
-# 
+#
 # The reason we're not using fonts is because of
 # https://github.com/emilk/egui/issues/3526
 
@@ -31,15 +31,16 @@ subprocess.run(["mkdir", "-p", "res/images/md-symbols"])
 
 for (group, icons) in ICONS.items():
    for name in icons:
+       outfile = "res/images/md-icons/{name}.png".format(name=name)
        args = ["convert",
                "{md_dir}/src/{group}/{name}/materialicons/24px.svg".format(md_dir=md_dir,
                                                                            group=str(group),
                                                                            name=name),
                "-density", "576",
-               "-background", "none", 
-               "-negate", "res/images/md-icons/{name}.png".format(name=name)]
-       print(args)
-       subprocess.run(args)	   
+               "-background", "none",
+               "-negate", outfile]
+       subprocess.run(args)
+       subprocess.run(["mogrify", "-strip", outfile])
 
 # Browse available symbols at https://fonts.google.com/icons
 SYMBOLS = [
@@ -47,15 +48,17 @@ SYMBOLS = [
         'file_open',
         'file_save',
         'new_window',
+        'piano',
         'play_arrow',
         'stop',
 ]
 for (symbol) in SYMBOLS:
+    outfile = "res/images/md-symbols/{symbol}.png".format(symbol=symbol)
     args = ["convert",
             "{md_dir}/symbols/web/{symbol}/materialsymbolssharp/{symbol}_wght100_24px.svg".format(md_dir=md_dir,
                                                                                                   symbol=symbol),
             "-density", "576",
-            "-background", "none", 
-            "-negate", "res/images/md-symbols/{symbol}.png".format(symbol=symbol)]
-    print(args)
-    subprocess.run(args)	   
+            "-background", "none",
+            "-negate", outfile]
+    subprocess.run(args)
+    subprocess.run(["mogrify", "-strip", outfile])
