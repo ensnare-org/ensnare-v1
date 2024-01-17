39e84e722722977315dcd81fc5d847ef01d68418
Mike Tsao <mike@sowbug.com>
2024-01-17 14:19:37 -0800

    delete old file

--- cores-egui/src/widgets/mod.rs
+++ cores-egui/src/widgets/mod.rs
@@ -1,8 +1,5 @@
 // Copyright (c) 2023 Mike Tsao. All rights reserved.
 
-/// Constants, structs, and enums for widgets.
-//pub mod parts;
-
 /// Contains widgets related to [Pattern](crate::mini::piano_roll::Pattern)s and
 /// [PianoRoll](crate::mini::piano_roll::PianoRoll).
 pub mod pattern;
