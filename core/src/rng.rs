ef6482c7c906445dd44e5501c83b673f7280ce43
Mike Tsao <mike@sowbug.com>
2024-01-29 10:19:20 -0800

    Clean up cargo/RNG

--- core/src/rng.rs
+++ core/src/rng.rs
@@ -2,21 +2,28 @@
 
 use std::time::{SystemTime, UNIX_EPOCH};
 
+/// A pseudorandom number generator (PRNG) for applications that don't require
+/// cryptographically secure random numbers. Pass the same number to
+/// [Rng::new_with_seed()] to get the same stream back again.
 #[derive(Debug)]
 pub struct Rng(oorandom::Rand64);
 impl Default for Rng {
     fn default() -> Self {
-        // This is an awful source of entropy, but it's fine for this use case
-        // where we just want a different fake struct each time.
-        Self(oorandom::Rand64::new(
-            SystemTime::now()
-                .duration_since(UNIX_EPOCH)
-                .unwrap()
-                .as_nanos(),
-        ))
+        // This is a poor source of entropy if we want the random-number stream
+        // to be unpredictable. It's fine for generating different test sets
+        // from run to run.
+        let seed = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_nanos();
+        Self::new_with_seed(seed)
     }
 }
 impl Rng {
+    pub fn new_with_seed(seed: u128) -> Self {
+        Self(oorandom::Rand64::new(seed))
+    }
+
     pub fn rand_u64(&mut self) -> u64 {
         self.0.rand_u64()
     }
@@ -41,7 +48,23 @@ mod tests {
     #[test]
     fn mainline() {
         let mut r = Rng::default();
-
         assert_ne!(r.rand_u64(), r.rand_u64());
+    }
+
+    #[test]
+    fn reproducible_stream() {
+        let mut r1 = Rng::new_with_seed(1);
+        let mut r2 = Rng::new_with_seed(2);
+        assert!(
+            (0..100).any(|_| r1.rand_u64() != r2.rand_u64()),
+            "RNGs with different seeds should produce different streams (or else you should play the lottery ASAP because you 2^6400 pairs of coin flips just came up the same)."
+        );
+
+        let mut r1 = Rng::new_with_seed(1);
+        let mut r2 = Rng::new_with_seed(1);
+        assert!(
+            (0..100).all(|_| r1.rand_u64() == r2.rand_u64()),
+            "RNGs with same seeds should produce same streams."
+        );
     }
 }
