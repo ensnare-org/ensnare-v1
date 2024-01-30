9cf1301b9d49c5696f1ff122234a9757e53a6fcd
Mike Tsao <mike@sowbug.com>
2024-01-30 09:18:41 -0800

    Notify composer when widget changes pattern

--- new-stuff/src/egui/composer.rs
+++ new-stuff/src/egui/composer.rs
@@ -21,77 +21,58 @@ use std::ops::RangeInclusive;
 
 #[derive(Debug)]
 pub struct ComposerWidget<'a> {
-    inner: &'a mut Composer,
+    composer: &'a mut Composer,
 }
 impl<'a> eframe::egui::Widget for ComposerWidget<'a> {
     fn ui(self, ui: &mut eframe::egui::Ui) -> eframe::egui::Response {
         ui.vertical(|ui| {
-            let mut response;
-            let new_pattern_response = ui.button("New Pattern");
-            if new_pattern_response.clicked() {
-                let _ = self.inner.add_pattern(
-                    PatternBuilder::default()
-                        .time_signature(self.inner.time_signature())
-                        .build()
-                        .unwrap(),
-                    None,
-                );
-            }
-            response = new_pattern_response;
-            let mut carousel_action = None;
-            let carousel_response = ui.add(CarouselWidget::widget(
-                &self.inner.ordered_pattern_uids,
-                &self.inner.patterns,
-                &mut self.inner.e.pattern_selection_set,
-                &mut carousel_action,
-            ));
-            if let Some(action) = carousel_action {
-                match action {
-                    CarouselAction::DeletePattern(pattern_uid) => {
-                        let _ = self.inner.remove_pattern(pattern_uid);
+            let r = {
+                let item_response = ui.button("New Pattern");
+                if item_response.clicked() {
+                    let _ = self.composer.add_pattern(
+                        PatternBuilder::default()
+                            .time_signature(self.composer.time_signature())
+                            .build()
+                            .unwrap(),
+                        None,
+                    );
+                }
+                item_response
+            } | {
+                let mut carousel_action = None;
+                let item_response = ui.add(CarouselWidget::widget(
+                    &self.composer.ordered_pattern_uids,
+                    &self.composer.patterns,
+                    &mut self.composer.e.pattern_selection_set,
+                    &mut carousel_action,
+                ));
+                if let Some(action) = carousel_action {
+                    match action {
+                        CarouselAction::DeletePattern(pattern_uid) => {
+                            let _ = self.composer.remove_pattern(pattern_uid);
+                        }
                     }
                 }
-            }
-            response |= carousel_response;
-            // if let Some(pattern_uid) = self
-            //     .inner
-            //     .e
-            //     .pattern_selection_set
-            //     .single_selection()
-            //     .cloned()
-            // {
-            //     if let Some(pattern) = self.inner.pattern_mut(pattern_uid) {
-            //         ui.label(format!("Time Signature: {}", pattern.time_signature()));
-            //         let pattern_edit_response = {
-            //             ui.set_min_height(256.0);
-            //             let desired_size = vec2(ui.available_width(), 96.0);
-            //             let (_id, rect) = ui.allocate_space(desired_size);
-            //             ui.add_enabled_ui(false, |ui| {
-            //                 ui.allocate_ui_at_rect(rect, |ui| {
-            //                     ui.add(GridWidget::widget(pattern.duration))
-            //                 })
-            //                 .inner
-            //             });
-            //             ui.allocate_ui_at_rect(rect, |ui| ui.add(PatternWidget::widget(pattern)))
-            //                 .inner
-            //         };
-            //         response |= pattern_edit_response;
-            //     }
-            // }
-            response |= ui.add(ComposerEditorWidget::widget(self.inner));
-
-            response
+                item_response
+            } | {
+                let item_response = ui.add(ComposerEditorWidget::widget(self.composer));
+                if item_response.changed() {
+                    self.composer.notify_pattern_change();
+                }
+                item_response
+            };
+            r
         })
         .inner
     }
 }
 impl<'a> ComposerWidget<'a> {
-    fn new(inner: &'a mut Composer) -> Self {
-        Self { inner }
+    fn new(composer: &'a mut Composer) -> Self {
+        Self { composer }
     }
 
-    pub fn widget(inner: &'a mut Composer) -> impl eframe::egui::Widget + 'a {
-        move |ui: &mut eframe::egui::Ui| ComposerWidget::new(inner).ui(ui)
+    pub fn widget(composer: &'a mut Composer) -> impl eframe::egui::Widget + 'a {
+        move |ui: &mut eframe::egui::Ui| ComposerWidget::new(composer).ui(ui)
     }
 }
 
